cf6bd6b32afc877a193493bf39b1089fc3f9e10f
Cyprian Rachel <cyprianrachel@interia.pl>
2021-03-18 18:14:46 +0100

    hello world

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xff7c94b6),
+                  image: const DecorationImage(
+                    image: AssetImage('Images/pazur.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 8,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+            ),
+
+
+            Expanded(
+              child: Container(
+
+                padding: EdgeInsets.all(30.0),
+                color: Colors.orange,
+                child: Text('2'),
+              ),
+            ),
+
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.green,
+                child: Text('3'),
+              ),
+            ),
+
+
+          ],
+
+
+        ),
+
+        appBar: AppBar(
+          title: Text('I am rich'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
